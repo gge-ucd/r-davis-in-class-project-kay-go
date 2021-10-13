b203ad7ad02aed1bad58950935bdd708c88b325c
Kay Garlick-Ott <oceanodroma@Kays-MacBook-Air.local>
2021-10-13 16:57:08 -0700

    wk3_hw

---
+++ scripts/week_3_homework.R
@@ -0,0 +1,27 @@
+#install.packages("tidyverse")
+#important dataframe
+surveys <- read.csv("data/portal_data_joined.csv")
+#inspect dataframe
+colnames(surveys)
+head(surveys)
+#rows 1 through 60, columns 6, 9 and 13, which correspond with species_id, weight, and plot_type
+surveys_base <- surveys[1:60,c(6,9,13)]
+surveys_base
+class("species_id")
+class("plot_type")
+?factor
+species_id_fac <- factor(surveys_base$species_id)
+is.factor(species_id_fac)
+plot_type_fac <- factor(surveys_base$plot_type)
+is.factor(plot_type_fac)
+#factors are numbers and they have an order... still trying to figure out what they mean
+#helps to think of the factor process as giving characters numerical values for the purpose of ordering them
+#like if all of your values in an Excel column were jumbled up. This would be a way that you could assign numbers to each character and then order them however you want (using further functions)
+surveys_base <- surveys_base[complete.cases(surveys_base),]
+surveys_base
+#had to peek on this last step here (above), because initially I tried to do na.omit, as I did on last week's homework.
+#my understanding is that na.omit removes nas across an entire dataframe (and creates a new one without?) whereas complete.cases goes row by row
+#how come I don't have to specify the column in the above function complete.cases(surveys_base),?
+challenge_base <- surveys_base[(surveys_base[,2]>150),]
+challenge_base
+#still not sure I understand why the comma matters
