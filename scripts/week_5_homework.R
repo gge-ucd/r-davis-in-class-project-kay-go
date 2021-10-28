51163fe39f6d5e11946ea9728b3e0bb0f6b7bfdf
Kay Garlick-Ott <oceanodroma@campus-012-099.ucdavis.edu>
2021-10-28 16:01:49 -0700

    updated 10/28

--- scripts/week_5_homework.R
+++ scripts/week_5_homework.R
@@ -18,12 +18,24 @@ surveys_1 <- select(surveys, genus, hindfoot_length, plot_type) %>%
     filter(!is.na(genus)) %>% filter(!is.na(plot_type)) %>% filter(!is.na(hindfoot_length)) %>% 
     group_by(genus, plot_type) %>% summarize(hindfoot_length_avg = mean(hindfoot_length))
 surveys_1
+#note from 10/28: select stage not needed, can go directly to filter after surveys %>% 
 #pivot so that each column is a plot type, values represent mean hindfoot length and genus are the rows
 surveys_wide <- surveys_1 %>% pivot_wider(names_from = "plot_type", values_from = "hindfoot_length_avg")
 surveys_wide
 #arrange so descending by control
 surveys_final <- arrange(surveys_wide, desc(Control))
 surveys_final
+
+#From class 10/28: What if we wanted to reverse this back into the longer version of what we made before
+?pivot_longer
+surveys_reverse <- surveys_wide %>%
+  pivot_longer(cols = c(Control:`Spectab exclosure`),
+               names_to = "plot_type",
+               values_to = "mean_hindfoot")
+# cols:  which columns I want to pivot
+# names_to: takes the column name and puts them into a column. What do you want to name the column of column names?
+# values_to: takes the values from each of these columns cells. What do you want to name the column of cell values?
+
 
 #ASSIGNMENT PART 2
 #Using the original, calculate a new weight category variable called weight_cat
